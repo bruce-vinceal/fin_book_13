467fdc4f574601365919f089b44a8c2a8e49c454
bruce-vinceal <brucevinceal@gmail.com>
2020-05-27 14:39:35 +0800

    updated dashboard-nav

--- resources/views/components/dashboard-nav.blade.php
+++ resources/views/components/dashboard-nav.blade.php
@@ -12,8 +12,8 @@
     <!-- Nav Item - User Information -->
     <li class="nav-item dropdown no-arrow">
       <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span class="mr-2 d-none d-lg-inline text-gray-600 small">User_Name</span>
-        <span class="logoutIcon"><i class="fas fa-sign-out-alt"></i></span>
+        <span class="mr-2 d-none d-lg-inline text-gray-600 small" id="user-full-name" name="user-f">User Full Name Here</span>
+        <span class="logoutIcon"><i class="fas fa-user-circle"></i></span>
       </a>
       <!-- Dropdown - User Information -->
       <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
