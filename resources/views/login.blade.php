3b7b1902633036e6c32999264308e62da0bf5a14
bruce-vinceal <brucevinceal@gmail.com>
2020-05-27 12:45:08 +0800

    finalize login page and css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,13 +5,13 @@
   <div class="login-container">
     <img class="mb-4" src="/img/logo.png" alt="" width="30%">
     <h1 class="mb-3">Login to Finote</h1>
-    <form class="form-login">
-          <label for="inputEmail" class="sr-only">Email address</label>
-          <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
-          <label for="inputPassword" class="sr-only">Password</label>
-          <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
-          <button class="btn btn-lg btn-primary btn-block my-3" type="submit">Log in</button>
-          <p class="my-3">Not a Member? <a href="/">Sign up for Finote.</a></p>
+    <form class="form-login" action="">
+          <label for="loginEmail" class="sr-only">Email address</label>
+          <input type="email" name="loginEmail" id="loginEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+          <label for="loginPwd" class="sr-only">Password</label>
+          <input type="password" name="loginPwd" id="loginPwd" class="form-control" placeholder="Password" required="">
+          <button class="btn btn-lg btn-primary btn-block my-3" id="login-btn" type="submit">Log in</button>
+          <p class="my-3">Not a Member? <a href="signup">Sign up for Finote.</a></p>
       </form>
     </div>
 </section>
