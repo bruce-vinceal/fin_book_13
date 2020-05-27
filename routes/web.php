e63f2222b082056666decfbea7d362cdaa5af76a
Euniel <manz_james@yahoo.com>
2020-05-27 23:04:57 +0800

    Putting Basic Controllers (login,signup,index) and web.php was updated

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/finote', 'GeneralController@index');
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/login', 'LoginController@show');
 
-Route::get('/signup', function () {
-    return view('signup');
-});
+Route::get('/signup', 'RegisterController@show');
 
 Route::get('/home', function () {
     return view('home');
