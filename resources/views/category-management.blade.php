230037cc4fda9f61c351cfec913ef1cd1d6a4fb5
bruce-vinceal <brucevinceal@gmail.com>
2020-05-27 22:15:03 +0800

    updated class of type in edit modal at category-management

--- resources/views/category-management.blade.php
+++ resources/views/category-management.blade.php
@@ -89,7 +89,7 @@
                                                     </div>
                                                 </div>
                                                 <div class="form-group row">
-                                                    <label for="editType" class="col-sm-3">Type</label>
+                                                    <label for="editType" class="col-sm-3 col-form-label">Type</label>
                                                     <div class="col-sm-9">
                                                         <select class="form-control" name="editType" id="editType">
                                                             <option value="Expense" selected>Expense</option>
