59c3b04e6800d1c51d3dab9b1a85276daa6d85b1
Euniel <manz_james@yahoo.com>
2020-05-28 22:49:49 +0800

    Added Ledger model and table

---
+++ database/migrations/2020_05_28_143623_create_ledger_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLedgerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ledger', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('category');
+            $table->string('description');
+            $table->string('amount');
+            $table->string('userid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ledger');
+    }
+}
