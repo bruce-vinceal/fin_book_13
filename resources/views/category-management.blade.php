dcceb7b1305f2743a89b2243a3fb519a1fdb0971
bruce-vinceal <brucevinceal@gmail.com>
2020-05-29 20:45:14 +0800

    table responsive category management

--- resources/views/category-management.blade.php
+++ resources/views/category-management.blade.php
@@ -53,68 +53,70 @@
         <div class="card-header">
         </div>
         <div class="card-body">
-            <table class="table" name="category-management-table" id="category-management-table">
-            <thead class="">
-                <tr>
-                <th class="column1" name="th-description" id="th-description">Description</th>
-                <th class="column2" name="th-type" id="th-type">Type</th>
-                <th class="column3" name="th-edit" id="th-edit"></th>
-                </tr>
-            </thead>
-            <tbody>
-              @foreach($user->categories as $category => $type)
-                <tr>
-                    <td class="column1" name="category-desciption" id="category-description"> {{ $category }} </td>
-                    <td class="column2" name="category-type" id="category-type"> {{ $type }} </td>
-                    <td class="column3" name="category-edit" id="category-edit">
-                        <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-sm btn-outline-primary" id="edit-category-btn" data-toggle="modal" data-target="#EditCategoryModal{{ $category }}">Edit
-                        </button>
+            <div class="table-responsive">
+                <table class="table" name="category-management-table" id="category-management-table">
+                    <thead class="">
+                        <tr>
+                        <th class="column1" name="th-description" id="th-description">Description</th>
+                        <th class="column2" name="th-type" id="th-type">Type</th>
+                        <th class="column3" name="th-edit" id="th-edit"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($user->categories as $category => $type)
+                        <tr>
+                            <td class="column1" name="category-desciption" id="category-description"> {{ $category }} </td>
+                            <td class="column2" name="category-type" id="category-type"> {{ $type }} </td>
+                            <td class="column3" name="category-edit" id="category-edit">
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btn btn-sm btn-outline-primary" id="edit-category-btn" data-toggle="modal" data-target="#EditCategoryModal{{ $category }}">Edit
+                                </button>
 
-                        <!-- Modal -->
-                        <div class="modal fade" id="EditCategoryModal{{ $category }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLongTitle">Edit Category</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true" style="font-size: 18px;"><i class="fas fa-times"></i></span>
-                                        </button>
+                                <!-- Modal -->
+                                <div class="modal fade" id="EditCategoryModal{{ $category }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLongTitle">Edit Category</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true" style="font-size: 18px;"><i class="fas fa-times"></i></span>
+                                                </button>
+                                            </div>
+                                            <form action="/finote/category/{{$user->id}}/{{$category}}" method="POST">
+                                            @csrf
+                                            {{ method_field('PATCH') }}
+                                            <div class="modal-body">
+                                                    <div class="form-group row">
+                                                        <label for="editDescription" class="col-sm-3 col-form-label">Description</label>
+                                                        <div class="col-sm-9">
+                                                            <input type="description" class="form-control" name="editCategory" id="editDescription" placeholder="{{ $category }}">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label for="editType" class="col-sm-3 col-form-label">Type</label>
+                                                        <div class="col-sm-9">
+                                                            <select class="form-control" name="editType" id="editType">
+                                                                <option value="Expense" {{($type == 'Expense')?'selected':''}}>Expense</option>
+                                                                <option value="Income" {{($type == 'Income')?'selected':''}}>Income</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                            </div>
+                                            
+                                            <div class="modal-footer">
+                                                <a href="/finote/category/{{$user->id}}/{{$category}}" class="btn btn-outline-danger">Delete</a>
+                                                <button type="submit" class="btn btn-primary" id="update-btn">Update</button>
+                                            </div>
+                                            </form>
+                                        </div>
                                     </div>
-                                    <form action="/finote/category/{{$user->id}}/{{$category}}" method="POST">
-                                    @csrf
-                                    {{ method_field('PATCH') }}
-                                      <div class="modal-body">
-                                              <div class="form-group row">
-                                                  <label for="editDescription" class="col-sm-3 col-form-label">Description</label>
-                                                  <div class="col-sm-9">
-                                                      <input type="description" class="form-control" name="editCategory" id="editDescription" placeholder="{{ $category }}">
-                                                  </div>
-                                              </div>
-                                              <div class="form-group row">
-                                                  <label for="editType" class="col-sm-3 col-form-label">Type</label>
-                                                  <div class="col-sm-9">
-                                                      <select class="form-control" name="editType" id="editType">
-                                                          <option value="Expense" {{($type == 'Expense')?'selected':''}}>Expense</option>
-                                                          <option value="Income" {{($type == 'Income')?'selected':''}}>Income</option>
-                                                      </select>
-                                                  </div>
-                                              </div>
-                                      </div>
-                                      
-                                      <div class="modal-footer">
-                                          <a href="/finote/category/{{$user->id}}/{{$category}}" class="btn btn-outline-danger">Delete</a>
-                                          <button type="submit" class="btn btn-primary" id="update-btn">Update</button>
-                                      </div>
-                                    </form>
                                 </div>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
-              @endforeach
-            </tbody>
-            </table>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 
