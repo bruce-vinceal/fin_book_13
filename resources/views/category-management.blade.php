6521bd83c1d76d1663b2bad736e26c6158b733e8
bruce-vinceal <brucevinceal@gmail.com>
2020-05-29 21:02:20 +0800

    updated css of ledger and category-management

--- resources/views/category-management.blade.php
+++ resources/views/category-management.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <!-- Begin Page Content -->
 <div class="container-fluid">
-    <div class="card">
+    <div class="card shadow">
         <div class="card-header">
                     <!-- Page Heading -->
             <div class="d-sm-flex align-items-center justify-content-between">
