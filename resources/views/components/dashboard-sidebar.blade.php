c392715f723bdce19a745260bb67e80f38d0dc84
Euniel <manz_james@yahoo.com>
2020-06-04 00:38:55 +0800

    Fixed Finote Logo Route

--- resources/views/components/dashboard-sidebar.blade.php
+++ resources/views/components/dashboard-sidebar.blade.php
@@ -2,7 +2,7 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/finote">
         <div class="sidebar-brand-icon">
         <span><i class="fas fa-book-open"></i></span>
         </div>
